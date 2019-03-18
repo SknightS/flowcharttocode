02117283e82a7324b355aca0386ccb2883b5697c
sakib <md.sakibrahman@gmail.com>
2019-03-18 15:13:40 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>FlowChart to Code Converter</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+
+    <div class="row">
+    <div class="col-md-3" style="background-color:lavender; height: 1000px" >Flowchart
+   <br><br><br> <button style="background-color: lightgreen;width: 70%" class="btn btn-sucess">Start</button>
+        <br><br>
+        <img height="10%" width="60%" src="image/flowchart-rectangle.jpg">
+        <br><br>
+        <img height="10%" width="60%" src="image/flowchart-parallel.jpg">
+        <br><br>
+        <img height="10%" width="60%" src="image/flowchart-oval.jpg">
+        <br><br>
+        <img height="10%" width="60%" src="image/flowchart-circle.jpg">
+        <br><br>
+        <img height="10%" width="60%"  src="image/flowchart-diamond.jpg">
+
+        <br><br><br> <button style="background-color: lightgreen;width: 70%" class="btn btn-sucess">CONVERT</button>
+
+    </div>
+
+    <div class="col-md-5" style="background-color:lavenderblush; height: 1000px"></div>
+
+    <div class="col-md-4" style="background-color:lavender; height: 1000px">Result</div>
+  </div>
+</div>
+
+</body>
+</html>
